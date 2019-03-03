dcd16d0fe230b994af84b5aaac91e50d9c2ca6eb
Dan Pastusek <dan@hexial.com>
2019-03-02 18:16:00 -0800

    working demo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,103 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ReactEcharts from 'echarts-for-react'
+
+import './App.css'
+
+const UPDATE_INTERVAL = 10 // milliseconds
+const SECONDS = 20 // how long to display the strip chart
+const UPDATES_PER_SECOND = 1000 / UPDATE_INTERVAL
+const SERIES_LENGTH = UPDATES_PER_SECOND * SECONDS
 
 class App extends Component {
+  state = {
+    amplitude: 1,
+    data: new Array(SERIES_LENGTH)
+  }
+
+  start = () => {
+    // Start running calcuations every {UPDATE_ITERVAL}ms
+    this.x = 0
+    this.interval = window.setInterval(this.update, UPDATE_INTERVAL)
+  }
+
+  pause = () => {
+    window.clearInterval(this.interval)
+  }
+
+  reset = () => {
+    this.x = 0
+    this.setState({ data: new Array(SERIES_LENGTH) })
+  }
+
+  update = () => {
+    const { data, amplitude } = this.state
+    this.x++
+
+    /* === DO CALCULATION BELOW === */
+
+    const value = Math.sin((this.x / UPDATES_PER_SECOND) * 10) * amplitude
+
+    /* === DO CALCULATION ABOVE === */
+
+    // remove first value, add last value
+    const newData = data.slice(1)
+    newData.push(value)
+    console.log(`X: ${this.x}`, `Y: ${value}`, newData)
+
+    this.setState({ data: newData })
+  }
+
+  getOption() {
+    return {
+      animation: false,
+      xAxis: {
+        type: 'category',
+        data: [...Array(SERIES_LENGTH).keys()].reverse()
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: this.state.data,
+          type: 'line',
+          smooth: true
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Drilling Simulator</h2>
+          <div className="controls">
+            <button onClick={() => this.start()}>start</button>
+            <button onClick={() => this.pause()}>pause</button>
+            <button onClick={() => this.reset()}>reset</button>
+            <input
+              type="number"
+              name="amplitude"
+              value={this.state.amplitude}
+              onChange={e => this.setState({ amplitude: e.target.value })}
+            />
+          </div>
         </header>
+        <section>
+          <ReactEcharts
+            option={this.getOption()}
+            notMerge={true}
+            lazyUpdate={true}
+            theme={'theme_name'}
+            onChartReady={this.onChartReadyCallback}
+            // onEvents={EventsDict}
+            // opts={{ renderer: 'svg' }}
+          />
+        </section>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
